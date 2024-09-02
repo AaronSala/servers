ac80ae5e2ab0d2dbf3dcba317f92ac2b08d77433
Aaron Sala <salaaron2@gmail.com>
2024-09-02 15:33:18 +0300

    db intergrations and routes

--- server.js
+++ server.js
@@ -1,45 +1,60 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const Blog = require("./models/blog");
 
 const app = express();
 
+//db connection
+let uri =
+  "mongodb+srv://salaaron:sala4492@cluster0.lfepu1i.mongodb.net/blogs?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(uri)
+  .then((result) =>
+    app.listen(3000, () => {
+      console.log("app listening on port 3000");
+    })
+  )
+  .catch((err) => console.log("error"));
+
+const morgan = require("morgan");
 //register view engine
 app.set("view engine", "ejs");
 
+//middleware static
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-//middleware
-app.use((req, res) => {
-  console.log('new request made')
-  console.log("host: ", req.hostname);
-  console.log("path: ", req.path);
-  console.log("method ", req.method);
-
-})
-
+//routes
 app.get("/", (req, res) => {
   //sending to a html page which is in the root directory of th eproject
 
-  const blogs = [
-    {
-      title: "Aaron coocked managu",
-      snippet:
-        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum",
-    },
-    {
-      title: "the lithos distractor",
-      snippet:
-        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum",
-    },
-    {
-      title: "Johns Mwanga",
-      snippet:
-        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum",
-    },
-  ];
-  res.render("index", { title: "home", blogs });
+  res.redirect("./blogs");
 });
 
 app.get("/about-us", (req, res) => {
   res.render("about", { title: "about" });
+});
+
+//blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    });
+});
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
@@ -48,8 +63,4 @@ app.get("/blogs/create", (req, res) => {
 
 app.use((req, res) => {
   res.status(404).render("404", { title: "error" });
-});
-
-app.listen(3000, () => {
-  console.log("app listening on port 3000");
 });
